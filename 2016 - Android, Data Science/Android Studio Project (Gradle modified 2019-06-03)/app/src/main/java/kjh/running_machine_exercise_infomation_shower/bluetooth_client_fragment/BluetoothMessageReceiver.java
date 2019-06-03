014406f690ae0314a0d8cf06b87211e72b0506bf
Kim Jin Hee <doskid2@naver.com>
2019-06-03 16:59:49 +0900

    compress because of internal file names are too long, can't be uploaded

--- 2016 - Android, Data Science/Android Studio Project (Gradle modified 2019-06-03)/app/src/main/java/kjh/running_machine_exercise_infomation_shower/bluetooth_client_fragment/BluetoothMessageReceiver.java
+++
@@ -1,6 +0,0 @@
-package kjh.running_machine_exercise_infomation_shower.bluetooth_client_fragment;
-
-public interface BluetoothMessageReceiver
-{
-    void use_received_message(String message);
-}
---
+++ 2016 - Android, Data Science/README.md
@@ -0,0 +1,1 @@
+The reason for the compression is that the internal file name is too long and can not be uploaded here.
